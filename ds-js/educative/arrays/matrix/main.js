759293147dc68673c2cca3cedde4e49373df9090
swopnilll <swopnilacharya@gmail.com>
2025-01-16 11:22:07 +1100

    Traverse Library Pattern

--- ds-js/educative/arrays/matrix/main.js
+++ ds-js/educative/arrays/matrix/main.js
@@ -76,3 +76,45 @@ const traverseMatrixBasedOnEvenOdd = (matrix) => {
     return storageArray;
 
 }
+
+// Example:
+
+// Given the library array
+
+const library = [
+    [1, 3, 5, 7],
+    [2, 4, 6, 8],
+    [9, 11, 13, 15]
+];
+
+// The correct zigzag traversal should return:
+
+// [9, 2, 1, 3, 4, 11, 13, 6, 5, 7, 8, 15]
+
+function traverseLibrary(library) {
+    let row = library.length - 1;
+    let col = 0;
+    let goingUp = true;
+    const result = [];
+
+    while (col < library[0].length) {
+        result.push(library[row][col]);
+        if (goingUp) {
+            if (row == 0) {
+                goingUp = false;
+                col += 1;
+            } else {
+                row -= 1;
+            }
+        } else {
+            if (row == library.length - 1) {
+                goingUp = true;
+                col += 1;
+            } else {
+                row += 1;
+            }
+        }
+    }
+
+    return result;
+}
